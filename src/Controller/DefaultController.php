ff5841ce9269834f12a7927819444641a12d0123
GDel84 <g-del@hotmail.fr>
2023-06-02 11:47:12 +0200

    add number article in panier

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,30 +2,41 @@
 
 namespace App\Controller;
 
+use App\Entity\Panier;
 use App\Repository\AccueilTopRepository;
 use App\Repository\ArticleCenterRepository;
 use App\Repository\ArticleRepository;
 use App\Repository\ArticleTopRepository;
+use App\Repository\PanierRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'accueil')]
-    public function index(AccueilTopRepository $accueilTopRepo): Response
+    public function index(AccueilTopRepository $accueilTopRepo, SessionInterface $session): Response
     {
+        $panier = $session->get("panier", []);
+        $nombreArticlesPanier = count($panier);
+
         return $this->render('accueil.html.twig', [
             'accueilTops' => $accueilTopRepo->findAll(),
+            'nombreArticlesPanier' => $nombreArticlesPanier,
         ]);
     }
     #[Route('/article', name: 'article')]
-    public function article(ArticleRepository $articleRepo, ArticleTopRepository $artTopRepo, ArticleCenterRepository $artCenterRepo): Response
+    public function article(ArticleRepository $articleRepo, ArticleTopRepository $artTopRepo, ArticleCenterRepository $artCenterRepo, SessionInterface $session): Response
     {
+        $panier = $session->get("panier", []);
+        $nombreArticlesPanier = count($panier);
+
         return $this->render('article.html.twig', [
             'article' => $articleRepo->findAll(),
             'articleTops' =>$artTopRepo->findAll(),
             'articleCenters' =>$artCenterRepo->findAll(),
+            'nombreArticlesPanier' => $nombreArticlesPanier,
         ]);
     }
     #[Route('/contact', name: 'contact')]
--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -12,6 +12,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PanierController extends AbstractController
@@ -34,11 +36,20 @@ class PanierController extends AbstractController
         ]);
     }
     #[Route('/panier', name:'panier')]
-    public function test(SessionInterface $session, ArticleRepository $articleRepo)
+    public function test(Request $request, SessionInterface $session, ArticleRepository $articleRepo, MailerInterface $mailer)
     {
+        //recuperation du formulaire panier
+        $panier = new Panier;
+        $form = $this->createForm(PanierFormType::class, $panier);
+        $form->handleRequest($request);
+
+        //mettre en memoir dans session les article choisie dans panier
         $panier = $session->get("panier", []);
 
         $dataPanier = [];
+
+        //compter le nombre d'article dans le panier
+        $nombreArticlesPanier = count($panier);
 
         foreach($panier as $id => $quantity){
             $article = $articleRepo->find($id);
@@ -46,9 +57,25 @@ class PanierController extends AbstractController
                 "article" => $article,
                 "quantite" => $quantity
             ];
-
         }
-        return $this->render('panier.html.twig', compact("dataPanier"));
+    
+        // Construire le contenu de l'e-mail
+        $message = $this->renderView('email/panier.html.twig', ['dataPanier' => $dataPanier]);
+    
+        // Créer l'objet Email
+        $email = (new Email())
+            ->from('test@mail.fr')
+            ->to('g-del@hotmail.fr')
+            ->subject('Devis pour le panier')
+            ->html($message);
+    
+        // Envoyer l'e-mail
+        $mailer->send($email);
+
+        return $this->render('panier.html.twig', [
+            'dataPanier' => $dataPanier,
+            'nombreArticlesPanier' => $nombreArticlesPanier,
+        ]);
     }
 
     #[Route('/add/{id}', name :'add')]
