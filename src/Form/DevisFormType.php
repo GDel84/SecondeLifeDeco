e4553104ea0ae0e72c306c23e2f6cb1df767d5bb
GDel84 <g-del@hotmail.fr>
2023-06-23 22:41:50 +0200

    design form devis

--- src/Form/DevisFormType.php
+++ src/Form/DevisFormType.php
@@ -4,8 +4,11 @@ namespace App\Form;
 
 use App\Entity\Devis;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,19 +17,45 @@ class DevisFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('lastName')
-            ->add('email', EmailType::class)
+            ->add('name', TextType::class, [
+                'attr' => ['class' => 'inputclass'],
+                'label' => 'Nom :',
+            ])
+            ->add('lastName', TextType::class, [
+                'attr' => ['class' => 'inputclass'],
+                'label' => 'Prénom :',
+            ])
+            ->add('email', EmailType::class,[
+                'attr' => ['class' => 'inputclass'],
+                'label' => 'e-mail :',
+            ])
             ->add('date', DateType::class, [
                 'widget' => 'choice',
-                'input'  => 'datetime_immutable'
+                'data' => new \DateTime(),
+                'label' => 'Date de début :',
+                'format' => 'dd-MM-yyyy', // Format inversé : jour-mois-année
             ])
             ->add('dateFin', DateType::class, [
                 'widget' => 'choice',
-                'input'  => 'datetime_immutable'
+                'data' => new \DateTime(),
+                'label' => 'Date de retour :',
+                'format' => 'dd-MM-yyyy', // Format inversé : jour-mois-année
             ])
-            ->add('livraison')
-            ->add('lieu')
+            ->add('livraison', ChoiceType::class, [
+                'attr' => ['class' => 'inputclass radio'],
+                'label' => 'Avec livraison ?',
+                'choices' => [
+                    'Oui' => true,
+                    'Non' => false,
+                ],
+                'multiple' => false, // Autoriser une seule sélection
+            
+            ])
+            ->add('lieu', TextType::class, [
+                'attr' => ['class' => 'inputclass'],
+                'label' => 'Lieu :',
+                
+            ])
         ;
     }
 
