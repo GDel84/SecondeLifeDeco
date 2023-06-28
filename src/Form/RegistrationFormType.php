8c077d3a22409919f5517049cc102fbb79537501
GDel84 <g-del@hotmail.fr>
2023-06-28 20:53:49 +0200

    add class in formtype

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -18,8 +19,14 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class)
-            ->add('email')
+            ->add('name', TextType::class, [
+                'attr' => ['class' => 'inputclass'],
+                'label' => 'Nom :',
+            ])
+            ->add('email', EmailType::class, [
+                'attr' => ['class' => 'inputclass'],
+                'label' => 'Email :',
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -44,6 +51,8 @@ class RegistrationFormType extends AbstractType
                         'max' => 4096,
                     ]),
                 ],
+                'attr' => ['class' => 'inputclass'],
+                'label' => 'Mot de passe :',
             ])
         ;
     }
--- src/Form/UserFormType.php
+++ src/Form/UserFormType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +14,16 @@ class UserFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('email')
+            ->add('name', TextType::class, [
+                'attr' => ['class' => 'inputclass'],
+                'label' => 'Nom :',
+            ])
+            ->add('email', EmailType::class, [
+                'attr' => ['class' => 'inputclass'],
+                'label' => 'Email :',
+            ])
         ;
     }
-
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
